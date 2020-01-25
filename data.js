12a2a4496808620b349dc7bad1c7b8b388663ad3
spencer-dale <spencerdale05@gmail.com>
2020-01-25 16:12:37 -0500

    added data file

---
+++ data.js
@@ -0,0 +1,41 @@
+var supplyChain = {
+	LU: "Land Usage",
+	FM: "Farm",
+	AF: "Animal Feed",
+	PR: "Processing",
+	TR: "Transport",
+	RE: "Retail",
+	PA: "Packaging"
+};
+
+var foodImpacts = {
+	beef1: 60,				// Beef herd (herds dedicated to beef production have greater emissions)
+	lamb: 24,
+	cheese: 21,
+	beef2: 21,				// Dairy herd
+	chocolate: 19,
+	coffee: 17,
+	prawns: 12,
+	palmOil: 8,
+	pigMeat: 7,
+	poultryMeat: 6,
+	oliveOil: 6,
+	fish1: 5, 				// Farmed
+	eggs: 4.5,
+	rice: 4,
+	fish2: 3, 				// Wild
+	milk: 3,
+	caneSugar: 3,
+	groundnuts: 2.5,
+	wheat: 1.4,
+	tomatoes: 1.4,
+	corn: 1,
+	cassava: 1,
+	soymilk: 0.9,
+	peas: 0.9,
+	bananas: 0.7,
+	rootVegetables: 0.4,
+	apples: 0.4,
+	citrusFruit: 0.3,
+	nuts: 0.3
+};
