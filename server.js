4f02389085122ee8a77fc0da21b8bb09b63b48cc
jlumbard <jlumbard@uwo.ca>
2020-01-25 17:35:02 -0500

    tested API

--- server.js
+++ server.js
@@ -18,4 +18,10 @@ app.post('/api/world', (req, res) => {
   );
 });
 
+app.post('/api/image', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
