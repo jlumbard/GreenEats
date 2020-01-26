cc06c6aa99ca89fbc07a671f5620efc6ce1f0c72
zain huda <mhuda@uwo.ca>
2020-01-25 21:37:49 -0500

    camrea work

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,15 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-import Home from './components/Home';
-import './App.css';
+import CameraHook from './hooks/CameraHook';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
-
-render() {
+render() { 
     return (
-      <Home />
+      <div className="App">
+        <CameraHook />
+      </div>
     );
   }
 }
 
 export default App;
+
