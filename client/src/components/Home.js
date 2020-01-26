472100c7d771b03fc0194c9503d22eb7ba4635f3
jennystella <jennystella.k@gmail.com>
2020-01-25 22:45:11 -0500

    Web Design (still in progress)
    not optimized for phones and nav bar contents are not to the right

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,27 +12,52 @@ import Nav from 'react-bootstrap/Nav';
 const styles = {
     mainImage: {
         height: 1356,
-
-        backgroundImage: `url(${"/images/banner1.jpg"})`
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${"/images/banner2edit2.jpg"})`
     }
+
   };
+const styleOther = {
+  navbarInside: {
+  background: 'white',
+  width: 'cover'
+}
+};
+
 
 class Home extends Component {
 render() {
     return (
       <div className="Home" style={styles.mainImage}>
       <Container>
-      <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Green Eats</Navbar.Brand>
+      <Navbar style={styleOther.navbarInside}>
+      <Navbar.Brand href="#home" >Green Eats</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-      <Nav.Link href="#home">About Us</Nav.Link>
+      <Nav.Link href="#home" >About Us</Nav.Link>
       <Nav.Link href="#howitworks">How It Works</Nav.Link>
       <Nav.Link href="#contact">Contact</Nav.Link>
 
     </Nav>
-
+    <Nav className="justify-content-end" activeKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   </Navbar.Collapse>
 </Navbar>
 
