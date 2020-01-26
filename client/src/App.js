cb03c9218fa8fe162af506f42fbf0483c59f86bf
jlumbard <jlumbard@uwo.ca>
2020-01-26 09:22:31 -0500

    switched to react chart.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ import Home from './components/Home';
 class App extends Component {
 render() {
     return (
-      <div>
       <Home />
-      </div>
     );
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,6 +10,7 @@ import FormControl from 'react-bootstrap/FormControl';
 import Nav from 'react-bootstrap/Nav';
 import CameraService from './CameraService';
 import Media from 'react-bootstrap/Media';
+import {Bar} from 'react-chartjs-2';
 
 
 
@@ -93,11 +94,27 @@ const bodyStyles = {
   }
 };
 
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
 
 class Home extends Component {
 render() {
     return (
       <div className="Home" style={styles.mainImage}>
+        
       <Container>
       <Row style = {firstrowStyles.firstRow}>
       <Nav className="mr-auto">
@@ -124,8 +141,27 @@ render() {
     <Container>
     <h1 style= {bodyStyles.header1}> <span style= {{backgroundColor: '#FFFFFF'}}>  Check your food, </span></h1>
     <h1 style= {bodyStyles.header2}> <span style= {{backgroundColor: '#FFFFFF'}}>start sustainable eating today </span> </h1>
-    <CameraService></CameraService>
+    <CameraService>
+
+    </CameraService>
     </Container>
+
+      <Container style={{backgroundColor:"white"}}>
+      <Bar
+          data={data}
+          width={100}
+          height={500}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+
+
+
+      </Container>
+
+
+
     </div>
       </div>
 
