d118faa886ab43fdadcf890ed605319dc120ebeb
jennystella <jennystella.k@gmail.com>
2020-01-25 21:37:31 -0500

    Homepage Web (still incomplete)
    need to resize background and edit the tab locations and input search
    in the middle of the page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
-import ToggleButton from 'react-bootstrap/ToggleButton';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Nav from 'react-bootstrap/Nav';
 
-class Home extends Component{
+const styles = {
+    mainImage: {
+        height: 1356,
 
+        backgroundImage: `url(${"/images/banner1.jpg"})`
+    }
+  };
+
+class Home extends Component {
 render() {
     return (
-    //  <ToggleButton value={1}>Option 1</ToggleButton>
-    <Button variant="primary" size="lg" disabled>
-        Primary button
-      </Button>
+      <div className="Home" style={styles.mainImage}>
+      <Container>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">Green Eats</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+      <Nav.Link href="#home">About Us</Nav.Link>
+      <Nav.Link href="#howitworks">How It Works</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+
+    </Nav>
+
+  </Navbar.Collapse>
+</Navbar>
+
+        {/*<Row>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+        </Row>
+
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>*/}
+        {/*<h1> TEST PLS WORK </h1>*/}
+
+      </Container>
+      </div>
+
     );
   }
 }
